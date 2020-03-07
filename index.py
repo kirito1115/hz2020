b163552dc504af4bfafd12b39e0d09796a6ce63f
Coldsama <Coldsama1213@163.com>
2020-03-07 22:22:15 +0800

    创建视图函数log

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 def index(request):
     return HttpResponse("hello world!")
+def log(request):
+    return redirect('/index')
